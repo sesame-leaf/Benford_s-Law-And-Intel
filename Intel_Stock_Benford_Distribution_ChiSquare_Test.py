9c88752b662f4cc15a7af28f024418bf05a169f3
sesame-leaf <bangjunseok2005@naver.com>
2025-06-16 15:05:42 +0900

    chore: Change more readable

--- Intel_Stock_Benford_Distribution_ChiSquare_Test.py
+++ Intel_Stock_Benford_Distribution_ChiSquare_Test.py
@@ -7,10 +7,16 @@ def main():
     # data length   10597
     # data type     np.float64
     intel_stock_data = np.load("data\\Intel_Stock_History_from_1980_03_17.npy")
+    
+    # data preprocess
     first_numbers = first_num(intel_stock_data)
     number_count = num_count(first_numbers)
     
-    print(p_value(Chi_square_val(number_count, benford()*np.sum(number_count)), 8))
+    chi_square_val = Chi_square_val(number_count, benford()*np.sum(number_count))
+    
+    p_val = p_value(chi_square_val, 8)
+    
+    print(p_val)
 
 
 main()
